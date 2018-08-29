022a8389ba4efd8f3485ba304cb99278262d06a6
Franverri <franverri@hotmail.com>
2018-08-29 10:44:40 -0300

    Lista de ciudades

--- app/src/main/java/tp0/weather_app/CitiesActivity.java
+++ app/src/main/java/tp0/weather_app/CitiesActivity.java
@@ -5,8 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CitiesActivity extends AppCompatActivity {
+
+    ListView listCities;
+    ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +21,30 @@ public class CitiesActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //Remove title bar
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        //Title bar name
+        getSupportActionBar().setTitle("Ciudades");
 
         setContentView(R.layout.activity_cities);
+
+        //Instance of ListView
+        listCities = (ListView) findViewById(R.id.listCities);
+        addCities();
+    }
+
+    private void addCities() {
+        //Obtain cities
+        String[] strCities = {
+                "Buenos Aires",
+                "Cordoba",
+                "Santa Fe",
+                "Chubut",
+                "Tucuman"
+        };
+
+        listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                strCities);
+
+        listCities.setAdapter(listAdapter);
     }
 }
