24f20cb14f16c131252c2b426f921064d1f8647f
Franverri <franverri@hotmail.com>
2018-08-29 11:17:43 -0300

    Argumentos entre actividades para determinar ciudad

--- app/src/main/java/tp0/weather_app/CitiesActivity.java
+++ app/src/main/java/tp0/weather_app/CitiesActivity.java
@@ -1,10 +1,14 @@
 package tp0.weather_app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,6 +33,27 @@ public class CitiesActivity extends AppCompatActivity {
         //Instance of ListView
         listCities = (ListView) findViewById(R.id.listCities);
         addCities();
+
+        //Hanlde item of listview click
+        listCities.setClickable(true);
+        listCities.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                //Object o = listView.getItemAtPosition(position);
+                // Realiza lo que deseas, al recibir clic en el elemento de tu listView determinado por su posicion.
+                Log.i("Click", "click en el elemento " + listAdapter.getItem(position) + " de mi ListView");
+                goMainActivity(listAdapter.getItem(position));
+            }
+        });
+    }
+
+    private void goMainActivity(String city) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("cityName", city);
+        startActivity(intent);
     }
 
     private void addCities() {
--- app/src/main/java/tp0/weather_app/MainActivity.java
+++ app/src/main/java/tp0/weather_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,8 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,20 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        //Get city name or put default as title name
+        Intent intent = getIntent();
+        Bundle extras = getIntent().getExtras();
+        if(intent.hasExtra("cityName")){
+            title =  extras.getString("cityName");
+        }else{
+            title = "Buenos Aires";
+        }
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
